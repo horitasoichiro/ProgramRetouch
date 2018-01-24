1f026461e271c99239f40ce2c08b527774a70dcb
LIKEIT貸出用 <horitasoichiro@gmail.com>
2018-01-24 19:03:38 +0900

    リファクタリング課題２・途中

--- EC/src/dao/BuyDAO.java
+++ EC/src/dao/BuyDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 import base.DBManager;
 import beans.BuyDataBeans;
@@ -90,6 +91,46 @@ public class BuyDAO {
 			System.out.println("searching BuyDataBeans by buyID has been completed");
 
 			return bdb;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new SQLException(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+	public static ArrayList<BuyDataBeans> getBuyDataBeansByUserId(int userId) throws SQLException {
+		Connection con = null;
+		PreparedStatement st = null;
+		try {
+			con = DBManager.getConnection();
+
+			st = con.prepareStatement(
+					"SELECT * FROM t_buy"
+							+ " JOIN m_delivery_method"
+							+ " ON t_buy.delivery_method_id = m_delivery_method.id"
+							+ " WHERE t_buy.user_id = ?");
+			st.setInt(1, userId);
+
+			ResultSet rs = st.executeQuery();
+
+			ArrayList<BuyDataBeans> userBuyList = new ArrayList<BuyDataBeans>();
+
+			while(rs.next()) {
+				BuyDataBeans bdb = new BuyDataBeans();
+				bdb.setId(rs.getInt("id"));
+				bdb.setTotalPrice(rs.getInt("total_price"));
+				bdb.setBuyDate(rs.getTimestamp("create_date"));
+				bdb.setDelivertMethodId(rs.getInt("delivery_method_id"));
+				bdb.setUserId(rs.getInt("user_id"));
+				bdb.setDeliveryMethodPrice(rs.getInt("price"));
+				bdb.setDeliveryMethodName(rs.getString("name"));
+			}
+
+			System.out.println("searching BuyDataBeans by buyID has been completed");
+
+			return userBuyList;
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 			throw new SQLException(e);
